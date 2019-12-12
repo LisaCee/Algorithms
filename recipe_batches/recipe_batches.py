42cb0bf8780ae43d6de22b7678d2bba37f9e4f9d
LisaCee <lisaceedesign@gmail.com>
2019-12-12 10:58:04 -0800

    recipe passing tests

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,27 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    needed = list(recipe.values())
+    have = list(ingredients.values())
+    total_batches = []
+    if len(have) != len(needed):
+        return 0
+    for amount in range(len(needed)):
+        if needed[amount] > have[amount]:
+            return 0
+        else:
+            batches = have[amount] // needed[amount]
+            print(batches)
+        total_batches.append(batches)
+    return min(total_batches)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
